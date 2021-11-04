bdd31578b36449f5641246f5da80e54088ba95f6
Josh Quan <joss4246@gmail.com>
2021-11-04 11:09:08 -0700

    First lose condition created and tested

--- midnight_rider.py
+++ midnight_rider.py
@@ -11,6 +11,10 @@ import midnight_rider_text
 #CONSTANTS
 MAX_FUEL = 50
 MAX_FOOD = 3
+MAX_HUNGER = 50
+ENDGAME_REASONS = {
+    "LOSE_AGENTS": 1,
+}
 
 class Game:
     """Represent our game engine
@@ -21,6 +25,12 @@ class Game:
         amount_of_sustenance: How much food we have left in our inventory
         agents_distance: describes the distance between the player and the agent.
         fuel: Describe the amount of fuel remaining, starts off at 50
+        hunger: describes how hungry thr player is, represented by a number between 0-50, if hunger goes beyond 50, game
+        is over.
+        hunger: describes how hungry our player is,
+        represented by a number between 0-50,
+        if hunger goes beyond 50, game is over
+        endgame_reason: shows the index of the game ending text from midnight_rider_text.py
     """
 
     def __init__(self):
@@ -29,6 +39,9 @@ class Game:
         self.amount_of_sustenance = MAX_FOOD
         self.agents_distance = -20
         self.fuel = MAX_FUEL
+        self.hunger = 0
+        self.endgame_reason = 0
+
 
     def introduction(self) -> None:
         """Print the introduction text"""
@@ -81,7 +94,7 @@ class Game:
             print(f"---------ZOOOOOOM.")
             print(f"----------You traveled {player_distance_now} km")
         elif user_choice == "b":
-            # Move the player
+            # Move the player slowly
             player_distance_now = random.randrange(5, 10)
             self.distance_traveled += player_distance_now
             # Move the agents
@@ -91,18 +104,58 @@ class Game:
             # Give the player some feedback
             print(f"---------You drive conservatively.")
             print(f"----------You traveled {player_distance_now} km")
+        elif user_choice == "a":
+            # consume one food if we have some available
+            if self.amount_of_sustenance > 0:
+                self.amount_of_sustenance -= 1
+
+                # Decrease hunger to 0
+                self.hunger = 0
+                # Give the player some feedback
+                print(midnight_rider_text.EAT_FOOD)
+            else:
+                print(midnight_rider_text.NO_FOOD)
+
+        if user_choice in ["b", "c", "d"]:
+            self.hunger += random.randrange(8, 18)
+
+
+    def upkeep(self) -> None:
+        """Give the user reminders of hunger"""
+        if self.hunger > 40:
+            print(midnight_rider_text.SEVERE_HUNGER)
+        elif self.hunger > 25:
+            print(midnight_rider_text.HUNGER)
+
+    def check_endgame(self) -> None:
+        """Check to see if win/lose conditions are met.
+        If they're met, change the self.done flag."""
+        if self.agents_distance >= 0:
+            # Allows us to quit the while loop
+            self.done = True
+            # Helps with printing the right ending
+            self.endgame_reason = ENDGAME_REASONS["LOSE_AGENTS"]
+
 def main() -> None:
     game = Game() # starting a new game
     # game.introduction()
 
     # Main Loop:
     while not game.done:
+        game.upkeep()
         # Display the choices to the player
         game.show_choices()
         # Ask the player what they want to do
         # Change the state of environment
         game.get_choice()
         # Check win/lose conditions
+        game.check_endgame()
+
+    time.sleep(2)
+    # Print out the ending
+    game.typewriter_effect(
+        midnight_rider_text.ENDGAME_TEXT[game.endgame_reason]
+    )
 
 if __name__ == "__main__":
     main()
--- midnight_rider_text.py
+++ midnight_rider_text.py
@@ -16,6 +16,7 @@ GON GETCHU.
 
 CHOICES = """
     --- Your Choices ---
+    A. Eat food
     B. Travel moderately
     C. Speed ahead at full speed
     D. Stop to refuel
@@ -28,3 +29,48 @@ REFUEL = """-------You filled the fuel tank.
 -------The agents got closer...
 """
 
+EAT_FOOD = """-------Mmmmm. Your hunger is sated
+"""
+
+NO_FOOD = """--------You have no food.
+"""
+
+SEVERE_HUNGER ="""**********Your stomach rumbles You need to eat something quick.
+"""
+HUNGER = """**********Your hunger is small but manageable.
+"""
+
+ENDGAME_TEXT = [
+    """The game has ended. Thanks for playing!""",     # Quit the game
+    """\n\nTHE AGENTS HAVE CLOSED IN ON YOU.
+THERE ARE AT LEAST AT LEAST CARS SURROUNDING YOU.
+THE LEAD CAR BUMPS YOUR PASSENGER SIDE.
+YOU MANAGE TO CORRECT YOUR STEERING
+TO KEEP YOU FROM CRASHING.
+
+YOU DIDN'T SEE THE AGENT'S CAR RIGHT BESIDE YOU.
+THAT DRIVER BUMPS YOUR CAR.
+AND THAT'S It.
+
+YOU SPIN UNCONTROLLABLY.
+YOUR CAR FLIPS OVER AT LEAST TWO TIMES.
+OR MORE... YOU SEEM TO HAVE LOST COUNT.
+
+SIRENS.
+
+"ARE THEY ALIVE?" THEY SAY AS YOU HEAR
+FOOTSTEPS COMING CLOSER.
+"DOESN'T MATTER, ALL WE WANTED WAS THE CAR."
+YOU SEE A DOG SLOWLY STEP OUT OF THE OVERTURNED CAR.
+
+"YOU WILL NEVER STOP THE REVOLUTION!
+THE DOG SEEMS TO SAY TO THE AGENTS.
+
+IT WAS IN THE CAR THE WHOLE TIME.
+
+YOU DRIFT OFF INTO UNCONSCIOUSNESS...
+
+---GAME OVER---
+\n""",      # LOSE - Agents have caught up
+
+]
